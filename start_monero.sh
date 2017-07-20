15f4746edd24bc27caab01d88faad734aad9e1d5
Giancarlo Franchini <gfranchini88@gmail.com>
2017-07-20 16:10:07 -0400

    added docker restart option

--- start_monero.sh
+++ start_monero.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker run -dit --name monero kannix/monero-miner -a cryptonight -o stratum+tcp://monerohash.com:3333 -u 45f25xoWSUvPdvDXCQjmEv7ihEceXF6nsVeyNtbavtmFQPgoUAtnKpDgX5w4jHV9dLizRVXLEkuGwBnaTMjDFwq4NTwgkM4 -p x -t 4
+docker run -d --restart=unless-stopped --name monero kannix/monero-miner -a cryptonight -o stratum+tcp://monerohash.com:3333 -u 45f25xoWSUvPdvDXCQjmEv7ihEceXF6nsVeyNtbavtmFQPgoUAtnKpDgX5w4jHV9dLizRVXLEkuGwBnaTMjDFwq4NTwgkM4 -p x -t 4
 docker logs -f monero
 
